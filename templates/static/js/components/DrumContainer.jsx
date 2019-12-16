26600602d8b30744086ae73f57cf226cd588fbb2
JKC90 <jkcostello90@gmail.com>
2019-12-16 13:14:54 -0500

    overhauling grid structure

--- templates/static/js/components/DrumContainer.jsx
+++ templates/static/js/components/DrumContainer.jsx
@@ -1,52 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import Grid from '@material-ui/core/Grid';
 
 const DrumContainer = props => {
     
-    let led_row = []
+    let sequence = []
     for(let i=0;i<16;i++){
-        led_row.push(
-                <div key={i} className = {`seq-step`}>{i+1}</div>
+        sequence.push(
+                <div key={i} className = {`seq-step drum-container-grid-items`}>
+                    <div key={i} className = {`led`}>{i+1}</div>
+                </div>
         )
     }
 
-    let sequence = []
-    let drum_selectors = [
-        [<div className="drum-selector-item" key="blank">
-            
-        </div>
-        ]
-    ]
+    let drum_selectors = [<div className="drum-selector-item drum-container-grid-items" key="blank"></div>]
+
+    let drum_buttons = [<div className="drum-button-item drum-container-grid-items" key="blank"></div>]
+    
+
     props.tracks.forEach ((t,i) => {
         let iter = i
         t.forEach ((n,i) => {
             sequence.push( 
-                <div key={`trig_${iter}_${i}`}>
+                <div key={`trig_${iter}_${i}`} className="drum-container-grid-items">
                     <ToggleButton className="sequence-item" selected={Boolean(n)} value="" onChange={(e)=>props.toggleDrum(iter,i)} > </ToggleButton>
                 </div>
             )
         })
         drum_selectors.push(
-            <div className="drum-selector-item" key={iter}>
+            <div className="drum-selector-item drum-container-grid-items" key={iter}>
                 <select value={props.drums[iter]} onChange={(e) => props.onSelectInstrument(e,iter)}>{props.selections}</select>
+            </div>
+        )
+        drum_buttons.push(
+            <div className="drum-button-item drum-container-grid-items" key={iter}>
+                Drum buttons
             </div>
         )
     })
 
     return(
-        <div className="drum-container">
-            <div className="drum-selector-container">
-                {drum_selectors}
-            </div>
-            <div className="sequence-container">
-                {led_row}
-                {sequence}
-            </div>
-            <div>
-                Last DIV omgg
-            </div>
+        <div className="drum-container ">
+            <Fragment>
+                <div className="drum-selector-container drum-container-grid">
+                    {drum_selectors}
+                </div>
+                <div className="sequence-container drum-container-grid">
+                    {sequence}
+                </div>
+                <div className="drum-selector-container drum-container-grid">
+                    {drum_buttons}
+                </div>
+            </Fragment>
         </div>
     )
 }
