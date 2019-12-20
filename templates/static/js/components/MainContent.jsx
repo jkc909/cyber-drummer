52de8d44562e190c0986e7f27aa71410ab6bb695
JKC90 <jkcostello90@gmail.com>
2019-12-20 13:11:48 -0500

    adding master volume and reverb controls

--- templates/static/js/components/MainContent.jsx
+++ templates/static/js/components/MainContent.jsx
@@ -6,43 +6,7 @@ import Grid from '@material-ui/core/Grid';
 import DrumContainer from './DrumContainer.jsx'
 import TransportContainer from './TransportContainer.jsx'
 import EqualizerContainer from './EqualizerContainer.jsx'
-
-const O = 12;
-
-const C = 0;
-const c = 1;
-const D = 2;
-const d = 3;
-const E = 4
-const F = 5;
-const f = 6;
-const G = 7;
-const g = 8;
-const A = 9;
-const a = 10;
-const B = 11;
-
-const S1 = 5 * O + E;
-const S2 = 4 * O + B;
-const S3 = 4 * O + G;
-const S4 = 4 * O + D;
-const S5 = 3 * O + A;
-const S6 = 3 * O + E;
-
-const X3 = 3 * O + G;
-const X4 = 3 * O + D;
-const X5 = 2 * O + A;
-const X6 = 2 * O + E;
-
-const _Em = [
-	S6 + 0
-	, S5 + 2
-	, S4 + 2
-	, S3 + 0
-	, S2 + 0
-	, S1 + 0
-];
-
+import MasterVolumeContainer from './MasterVolumeContainer.jsx'
 
 class MainContent extends Component {
 
@@ -67,6 +31,8 @@ class MainContent extends Component {
             data:[],
             bpm:120,
             eq_values: [10,-3,13,0,10,9,10,20,10,-3],
+            reverb_value: .1,
+            master_volume: .5,
             
         };
         this.beats=[];
@@ -78,6 +44,8 @@ class MainContent extends Component {
         this.handleVolumeChange=this.handleVolumeChange.bind(this);
         this.handleVolumeState=this.handleVolumeState.bind(this);
         this.handleEqSlider=this.handleEqSlider.bind(this);
+        this.handleReverbChange=this.handleReverbChange.bind(this);
+        this.handleMasterVolumeChange=this.handleMasterVolumeChange.bind(this)
     };
 
     componentDidMount(){
@@ -92,7 +60,9 @@ class MainContent extends Component {
 		this.midiSounds.setBand4k(this.state.eq_values[7]);
 		this.midiSounds.setBand8k(this.state.eq_values[8]);
 		this.midiSounds.setBand16k(this.state.eq_values[9]);
-        this.midiSounds.setEchoLevel(.1);
+        this.midiSounds.setEchoLevel(this.state.reverb_value);
+        this.midiSounds.setMasterVolume(this.state.master_volume);
+
     };
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -184,9 +154,13 @@ class MainContent extends Component {
         this.fillBeat();
     };
 
-    echoToggle(){
-        this.midiSounds.setEchoLevel(.1);
+    handleReverbChange(val){
+        this.midiSounds.setEchoLevel(val);
     };
+
+    handleMasterVolumeChange(val){
+        this.midiSounds.setMasterVolume(val);
+    }
 
     setBpm(bpm){
         this.state.bpm = bpm
@@ -239,16 +213,12 @@ class MainContent extends Component {
                             eq_values={this.state.eq_values}
                             handleEqSlider={this.handleEqSlider}
                         />
-                        <div className="master-volume">
-                            <div>
-                                <input className='eq-slider' type="range" min={0} max={100} value={50} onChange={e=>(e)}></input>
-                            </div>
-                            <div>
-                                <input className='eq-slider' type="range" min={0} max={100} value={50} onChange={e=>(e)}></input>
-                            </div>
-                            <div>Master Reverb</div>
-                            <div>Master Volume</div>
-                        </div>
+                        <MasterVolumeContainer 
+                            reverb_value={this.state.reverb_value}
+                            handleReverbChange={this.handleReverbChange}
+                            master_volume={this.state.master_volume}
+                            handleMasterVolumeChange={this.handleMasterVolumeChange}
+                        />
                     </div>
                 </Grid>
                 <div>
