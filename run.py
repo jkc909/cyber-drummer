5e1bd1aa40afc0056f58b2ef875799ec5c3e81aa
JKC90 <jkcostello90@gmail.com>
2019-12-20 20:32:23 -0500

    heroku deploy

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from templates import app
 
 if __name__ == '__main__':
-    app.config.from_object('configurations.DevelopmentConfig')
-    app.run()
+    app.config.from_object('configurations.ProductionConfig')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
